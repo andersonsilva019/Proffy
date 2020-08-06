da5430665f3628766edcdc2f445c55961da57b23
Anderson0017 <andeerson0017@gmail.com>
2020-08-06 17:25:56 -0300

    create page giveClasses

--- mobile-proffys/src/screens/GiveClasses/index.tsx
+++ mobile-proffys/src/screens/GiveClasses/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, ImageBackground } from 'react-native';
+
+import giveClassesBg from '../../assets/images/give-classes-background.png';
+
+import { styles } from '../GiveClasses/styles';
 
 const GiveClasses: React.FC = () => {
+
+  const { goBack } = useNavigation();
+
   return (
-    <Text>GiveClasses</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode='contain'
+        source={giveClassesBg}
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um proffy ?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+        </Text>
+
+      </ImageBackground>
+
+      <RectButton onPress={() => goBack()} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
+    </View>
   );
 }
 
